54453acdfe322871fa425267a54fe9664c461985
Lawrence <klawrence026@gmail.com>
2023-04-25 10:03:13 +0200

    Fixed Both

--- app.py
+++ app.py
@@ -30,17 +30,17 @@ def productDescription():
 
 
 
-@app.route('/job-description', methods=["GET", "POST"])
+@app.route('/blog-post', methods=["GET", "POST"])
 def jobDescription():
 
     if request.method == 'POST':
-        query = request.form['jobDescription']
+        query = request.form['blogPost']
         print(query)
 
         prompt = 'AI Suggestions for {} are:'.format(query)
         openAIAnswer = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-    return render_template('job-description.html', **locals())
+    return render_template('blog-post.html', **locals())
 
 
 
